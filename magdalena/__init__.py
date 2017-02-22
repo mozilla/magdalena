c4d3bd89b23a1476cff7c173cc9dbaa558e86709
Calixte Denizet <calixte.denizet@gmail.com>
2017-02-22 15:45:33 +0100

    Enable cors

--- magdalena/__init__.py
+++ magdalena/__init__.py
@@ -3,6 +3,7 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -11,27 +12,33 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/categories', methods=['GET'])
+@cross_origin()
 def categories():
     from magdalena import api
     return api.categories()
 
 
 @app.route('/bytypes', methods=['GET'])
+@cross_origin()
 def bytypes():
     from magdalena import api
     return api.bytypes()
 
 
 @app.route('/annotations', methods=['GET', 'POST'])
+@cross_origin()
 def annotations():
     from magdalena import api
     return api.annotations()
 
 
 @app.route('/update', methods=['GET'])
+@cross_origin()
 def update():
     from magdalena import api
     return api.update()
