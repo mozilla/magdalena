f7f9174e52e2c352dcc3cdc404345e035b324169
Calixte Denizet <calixte.denizet@gmail.com>
2017-02-22 14:15:28 +0100

    Don't track SQLAlchemy modifs

--- magdalena/__init__.py
+++ magdalena/__init__.py
@@ -9,6 +9,7 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
