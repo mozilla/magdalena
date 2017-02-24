6060a326e1845417094281418fcf5bcff587da4f
Calixte Denizet <calixte.denizet@gmail.com>
2017-02-24 19:56:11 +0100

    Add a script to create the db

---
+++ bin/create.py
@@ -0,0 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import logging
+from magdalena import models
+
+
+logging.basicConfig(level=logging.INFO)
+models.fill_tables()
