d6fa0ae865c140992729f31db5b1607eee74bce3
philipp <philipp-sumo@users.noreply.github.com>
2017-04-11 17:45:16 +0200

    Update dashboard.js
    fix a typo to correct the links for the startup crash graphs link (crashes by category)

--- static/dashboard/dashboard.js
+++ static/dashboard/dashboard.js
@@ -204,7 +204,7 @@ function make_link(parent, text, field, product, channel) {
     if (field === "rateBrCo" || field === "startup") {
         var pre = (product === "Firefox") ? "fx" : "and",
             chan = (channel === "release") ? "rel" : channel,
-            post = (field === "startup") ? "-bycat" : "",
+            post = (field === "startup") ? "-bcat" : "",
             a = $("<a></a>");
         a.attr("href", "longtermgraph/index.html?" + pre + chan + post)
          .attr("target", "_blank")
